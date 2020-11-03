eecdfd8c10a30a14bc6753df295f5876fa5bcd32
Tepes Alexandru <godzplayer@hotmail.com>
2020-11-03 14:50:19 +0200

    Day 1 - Second And Third sample solved

---
+++ Exercises2_Day_1/Exercises2_Day_1/LinkedList.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises2_Day_1
+{
+    class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int d)
+        {
+            value = d;
+            next = null;
+        }
+    }
+    class LinkedList
+    {
+        public Node head = null;
+        public void Print()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.value + " ");
+                temp = temp.next;
+            }
+        }
+        public void InsertNode(int x)
+        {
+            Node new_node = new Node(x);
+            if (head == null)
+            {
+                head = new_node;
+                return;
+            }
+            Node last = GetLastNode();
+            last.next = new_node;
+        }
+        public Node GetLastNode()
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void Reverse()
+        {
+            Node prev = null;
+            Node current = head;
+            Node temp = null;
+
+            while (current != null)
+            {
+                temp = current.next;
+                current.next = prev;
+                prev = current;
+                current = temp;
+            }
+            head = prev;
+        }
+        private Node Merge(Node a, Node b)
+        {
+            Node result = null;
+
+            if (a == null)
+            {
+                return b;
+            } else if (b == null)
+            {
+                return a;
+            }
+
+            if (a.value <= b.value)
+            {
+                result = a;
+                result.next = Merge(a.next, b);
+            } else
+            {
+                result = b;
+                result.next = Merge(a, b.next);
+            }
+            return result;
+        }
+        private void Split(Node source, LinkedList front, LinkedList back)
+        {
+            Node fast, slow;
+            slow = source;
+            fast = source.next;
+
+            while (fast != null)
+            {
+                fast = fast.next;
+                if (fast != null)
+                {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+            }
+
+            front.head = source;
+            back.head = slow.next;
+            slow.next = null;
+
+        }
+        private void MergeSort(LinkedList l)
+        {
+            Node h = l.head;
+            LinkedList a = new LinkedList();
+            LinkedList b = new LinkedList();
+
+            if ((h == null || h.next == null))
+            {
+                return;
+            }
+
+            Split(h, a, b);
+
+            MergeSort(a);
+            MergeSort(b);
+
+            l.head = Merge(a.head, b.head);
+
+        }
+        public void Sort()
+        {
+            MergeSort(this);
+        }
+        public int GetNthElementFromEnd(int n)
+        {
+            Node main = head;
+            Node forward = head;
+           
+            for (int i = 0; i < n; i++)
+            {
+                if (forward == null) return -1;
+                forward = forward.next;
+            }
+
+            while (forward != null)
+            {
+                main = main.next;
+                forward = forward.next;
+            }
+
+            return main.value;
+            
+        }
+    }
+}
---
+++ Exercises2_Day_1/Exercises2_Day_1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Exercises2_Day_1
+{
+    class Program
+    {
+        
+        static int FromRomanToArab(string s)
+        {
+            Dictionary<char, int> value = new Dictionary<char, int>();
+            value.Add('I', 1);
+            value.Add('V', 5);
+            value.Add('X', 10);
+            value.Add('L', 50);
+            value.Add('C', 100);
+            value.Add('D', 500);
+            value.Add('M', 1000);
+
+            int len = s.Length;
+            int n = 0, c = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (i < len - 1 && value[s[i]] < value[s[i + 1]])
+                {
+                    c = value[s[i + 1]] - value[s[i]];
+                    i++;
+                } else
+                {
+                    c = value[s[i]];
+                }
+                n += c;
+            }
+
+            return n;
+
+        }
+        static string FromArabToRoman(int n)
+        {
+            Dictionary<int, string> value = new Dictionary<int, string>();
+            value.Add(1, "I");
+            value.Add(4, "IV");
+            value.Add(5, "V");
+            value.Add(9, "IX");
+            value.Add(10, "X");
+            value.Add(40, "XL");
+            value.Add(50, "L");
+            value.Add(90, "XC");
+            value.Add(100, "C");
+            value.Add(400, "CD");
+            value.Add(500, "D");
+            value.Add(900, "CM");
+            value.Add(1000, "M");
+
+            string s = "";
+            int idx = 12;
+            
+            while (n != 0)
+            {
+                int div = n / value.ElementAt(idx).Key;
+                n = n % value.ElementAt(idx).Key;
+                while (div != 0)
+                {
+                    s += value.ElementAt(idx).Value;
+                    div--;
+                }
+                idx--;
+            }
+            return s;
+
+        } 
+        static bool IsPalindrome(LinkedList l)
+        {
+            LinkedList simmetric = new LinkedList();
+
+            Node temp = l.head;
+            while (temp != null)
+            {
+                simmetric.InsertNode(temp.value);
+                temp = temp.next;
+            }
+
+            simmetric.Reverse();
+
+            while (l.head.next != null)
+            {
+                if (l.head.value != simmetric.head.value)
+                {
+                    return false;
+                }
+                //Console.WriteLine(l.head.value + " " + simmetric.head.value);
+                l.head = l.head.next;
+                simmetric.head = simmetric.head.next;
+            }
+            return true;
+        }
+        static void Hanoi(int n, char from, char to, char aux)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine($"Move disk 1 from {from} to {to}");
+                return;
+            }
+            Hanoi(n - 1, from, aux, to);
+            Console.WriteLine($"Move disk {n} from {from} to {to}");
+            Hanoi(n - 1, aux, to, from);
+        }
+
+        static void Main(string[] args)
+        {
+            LinkedList x = new LinkedList();
+            x.InsertNode(1);
+            x.InsertNode(3);
+            x.InsertNode(3);
+            x.InsertNode(5);
+            x.InsertNode(2);
+            x.InsertNode(3);
+            x.InsertNode(3);
+            x.InsertNode(5);
+            x.InsertNode(1);
+
+            x.Sort();
+            x.Print();
+
+            Console.WriteLine();
+            Console.WriteLine(x.GetNthElementFromEnd(7));
+
+            Console.ReadKey();
+        }
+    }
+}
